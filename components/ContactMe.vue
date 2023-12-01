08edad4d398739a91811c7de7fbd9312bf595fa9
ROMY Ulvy <ulvy@chamroeun.cam>
2023-12-01 15:50:11 +0700

    Merged remote changes

--- components/ContactMe.vue
+++ components/ContactMe.vue
@@ -1,28 +1,22 @@
 <template>
-<div class="w-full">
+<div id="contact" class="w-full">
     <h1 class="text-[50px] gradient underline md:text-[40px] sm:text-[30px] lg:text-[50px] xl:text-[50px] font-[600]">Contact Me</h1>
     <p class="text-[20px] ml-2">Wanna keep in touch with me :</p>
-    <div class="ml-6 mt-4 flex-col">
+    <div class="md:ml-6 lg:ml-6 xl:ml-6 sm:ml-5 mt-4 flex-col w-full ">
         <div class="grid grid-cols-2 place-items-center ">
             <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5l-8-5h16zm0 12H4V8l8 5l8-5v10z"/></svg> -->
-            <b class="gradient font-[600] text-[18px] w-[10%]">Email : </b>
+            <b class="gradient font-[600] text-[18px] lg:w-[10%] xl:w-[10%] md:w-[10%] sm:w-[28%]">Email : </b>
             <a href="mailto:ulvyromy156@gmail.com" class="text-[17px] hover:underline"> ulvyromy156@gmail.com</a>
         </div>
 
         <div class="grid grid-cols-2 place-items-center my-2">
             <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5l-8-5h16zm0 12H4V8l8 5l8-5v10z"/></svg> -->
-            <b class="gradient font-[600] text-[18px] w-[10%]">Phone : </b>
-            <a href="tel:+855889406902" class="text-[17px] hover:underline"> 0889406902</a>
-        </div>
-
-        <div class="grid grid-cols-2 place-items-center my-2">
-            <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5l-8-5h16zm0 12H4V8l8 5l8-5v10z"/></svg> -->
-            <b class="gradient font-[600] text-[18px] w-[10%]">Telegram : </b>
-            <a href="tg://resolve?domain=https://t.me/AsTrAy15" class="text-[17px] hover:underline">077257675</a>
+            <b class="gradient font-[600] text-[18px] lg:w-[10%] xl:w-[10%] md:w-[10%] sm:w-[28%]">Telegram : </b>
+            <a href="tg://resolve?domain=https://t.me/AsTrAy15" class="text-[17px] hover:underline">Astray</a>
         </div>
         <div class="grid grid-cols-2 place-items-center my-2">
             <!-- <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 5l-8-5h16zm0 12H4V8l8 5l8-5v10z"/></svg> -->
-            <b class="gradient font-[600] text-[18px] w-[10%]">LinkedIn : </b>
+            <b class="gradient font-[600] text-[18px] lg:w-[10%] xl:w-[10%] md:w-[10%] sm:w-[28%]">LinkedIn : </b>
             <a href="https://www.linkedin.com/in/ulvy-romy-218157231?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" class="text-[17px] hover:underline">Ulvy Romy</a>
         </div>
         
